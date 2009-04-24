6be56eb177610826e9ab059e2cdc569c7df2ebe4
Andrew Shirley <andrew@als-bi.com>
2009-04-24 18:50:36 +0100

    put a date in the pak filename

--- makeDslMod.sh
+++ makeDslMod.sh
@@ -1,7 +1,10 @@
 cd build/release-linux-i386/baseq3/
-zip -r ../../../zzz-dslmod.pk3 vm 
+zip -r ../../../zzz-dslmod-`date +"%Y%m%d"`.pk3 vm 
 cd ../../..
 
 cd media
-zip -r ../zzz-dslmod.pk3 *
+zip -r ../zzz-dslmod-`date +"%Y%m%d"`.pk3 *
+
+echo
+echo zzz-dslmod-`date +"%Y%m%d"`.pk3
 
