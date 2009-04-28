0c716b931e7cfa0bf67a5a5e655587ae0ce28ada
Andrew Shirley <andrew@als-bi.com>
2009-04-28 13:28:55 +0100

    un-dated mods take precidence over dated ones, add another z to prevent this!

--- makeDslMod.sh
+++ makeDslMod.sh
@@ -1,9 +1,9 @@
 cd build/release-linux-i386/baseq3/
-zip -r ../../../zzz-dslmod-`date +"%Y%m%d"`.pk3 vm 
+zip -r ../../../zzzz-dslmod-`date +"%Y%m%d"`.pk3 vm 
 cd ../../..
 
 cd media
-zip -r ../zzz-dslmod-`date +"%Y%m%d"`.pk3 *
+zip -r ../zzzz-dslmod-`date +"%Y%m%d"`.pk3 *
 
 echo
 echo zzz-dslmod-`date +"%Y%m%d"`.pk3
